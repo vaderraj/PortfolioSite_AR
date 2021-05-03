97dd10671e414f7af706b27bd45dc386155c717e
Aman Raj <amanrajonworld@gmail.com>
2021-05-03 12:32:18 +0530

    Initial commit for frontend: boilerplate code added

---
+++ Frontend/portfolio_frontend/src/routes/primary.routes.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const PrimaryRoutes = props => {
+    return(
+        <Fragment>               
+                    <div>
+                        <h1>
+                            Start of Page
+                        </h1>
+                    </div>            
+        </Fragment>
+    );
+};
+
+export default PrimaryRoutes;
